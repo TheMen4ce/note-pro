8f6370e91e3cc823e9c851b6bad2218b69669c84
Dennis <dennisbriner@gmail.com>
2017-10-01 18:18:14 +0200

    implemented style changer

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,8 @@
+$('.style-selector').change(function(){
+    let stylesrc = '/css/style/'+$(this).val()+'.css';
+    $('#style-css').attr('href',stylesrc);
+})
+
 $('.btn-expand').click(function (btn) {
     $(this).toggleClass('clicked');
     $(this).parent().parent().parent().find('.expandable').toggleClass('expanded', [2]);
