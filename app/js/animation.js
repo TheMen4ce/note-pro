d5c9d6a42b7e449f9ed01149c5b856ffea3120c5
Dennis <dennisbriner@gmail.com>
2017-10-08 13:48:48 +0200

    added rating, added gridlayout for notes, added notes in app.js

--- app/js/animation.js
+++ app/js/animation.js
@@ -3,6 +3,7 @@
 function init(){
     if(localStorage.style){
         changeStyle(localStorage.style);
+        $('.style-selector').val(localStorage.style);
     }
 }
 init();
@@ -18,9 +19,9 @@ $('.style-selector').change(function(){
 
 $('.btn-expand').click(function (btn) {
     $(this).toggleClass('clicked');
-    $(this).parent().parent().parent().find('.expandable').toggleClass('expanded', [2]);
+    $(this).parent().parent().parent().find('.note-text').toggleClass('expanded', [2]);
 });
 
 $('.btn-edit').click(function () {
-    $('.expandable').focus();
+    $('.note-text').focus();
 });
---
+++ app/js/app.js
@@ -0,0 +1,26 @@
+var notes = [
+    {
+        id: 1,
+        finished: false,
+        rating: 3,
+        title: "Das ist der Title",
+        content: "Sali s'eint, sali s'ander, fertig lustig!",
+        doDate: new Date()
+    },
+    {
+        id: 2,
+        finished: true,
+        rating: 3,
+        title: "Am Mami helfe poschtä",
+        content: "Alüte, fräge wies gaht, alli Läde abklappere, etc.",
+        doDate: new Date()
+    },
+    {
+        id: 3,
+        finished: false,
+        rating: 3,
+        title: "Über d'Strass laufe",
+        content: "Luege, Lose, Laufe! hähä",
+        doDate: new Date()
+    }
+]
