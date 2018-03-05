5bc81e5c6c700c7f137f51467a49913ad4372899
Dennis <dennisbriner@gmail.com>
2018-03-05 21:16:17 +0100

    updated moment, removed moment from node_modules, added preload

--- index.js
+++ index.js
@@ -14,5 +14,5 @@ app.use(require("method-override")(function (req) {
 app.use(require('./server/routes/notesRoutes.js'));
 app.use(express.static(__dirname + '/public'));
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 app.listen(port, () => { console.log(`Server running at http://localhost:${port}/`); });
